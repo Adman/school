661ea677694e214f819c362622fa33d2087cb8c3
Adrian Matejov <a.matejov@centrum.sk>
2016-11-22 23:55:38 +0100

    db: bonus homework
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

---
+++ databases/bonus_du.sql
@@ -0,0 +1,26 @@
+WITH lower_price AS (
+    SELECT c2.Krcma, c2.Alkohol
+    FROM capuje c, capuje c2
+    WHERE c.Alkohol=c2.Alkohol AND c.Cena < c2.Cena
+)
+WITH lowest_price AS (
+    SELECT c.Krcma, c.Alkohol
+    FROM capuje c
+    WHERE NOT EXISTS(SELECT * FROM lower_price lp
+                     WHERE lp.Alkohol=c.Alkohol AND lp.Krcma=c.Krcma)
+)
+WITH failed_drunkard AS (
+    SELECT DISTINCT n.Pijan
+    FROM navstivil n, navstivil n2, vypil v, vypil v2, lowest_price lp
+    WHERE n.Pijan=n2.Pijan AND n.Id < n2.Id AND n.Id=v.Id AND
+          n2.Id=v2.Id AND v.Alkohol=v2.Alkohol AND lp.Alkohol=v.Alkohol AND
+          lp.Krcma != n2.Krcma
+)
+WITH failed_pub (
+    SELECT DISTINCT n.Krcma
+    FROM navstivil n, failed_drunkard fd
+    WHERE n.Pijan=fd.Pijan
+)
+SELECT DISTINCT n.Krcma
+FROM navstivil n
+WHERE NOT EXISTS(SELECT * FROM failed_pub f WHERE f.Krcma=n.Krcma);
