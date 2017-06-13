cfb36ef415d84f77ec0a879d952a980de0f75fa2
Adrian Matejov <a.matejov@centrum.sk>
2017-06-13 20:55:47 +0200

    pts: removed random
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- pts/ludo/test.py
+++ pts/ludo/test.py
@@ -1,50 +1,52 @@
 import unittest
-import random
 from ludo import Ludo, Player, FIELDS, MAX_POINTS
 
 
 class LudoTest(unittest.TestCase):
-    def get_ludo(self):
+    def get_ludo(self, players_pieces=[]):
         players = []
+        if not players_pieces:
+            players_pieces = [[1, 10, 20, 30],
+                              [5, 15, 25, 35],
+                              [2, 14, 11, 28],
+                              [39, 41, 42, 43]]
+
         for i in range(4):
-            pieces = [random.randint(0, FIELDS), random.randint(0, FIELDS),
-                      random.randint(0, FIELDS), random.randint(0, FIELDS)]
-            players.append(Player(pieces=pieces))
-        return Ludo(players=players)
+            players.append(Player(pieces=players_pieces[i]))
+        return Ludo(players=tuple(players))
 
     def test_move(self):
-        dice = random.randint(1, 6)
+        dice = 5
         l1 = self.get_ludo()
         l2, _ = l1.move(0, dice)
 
-        self.assertEqual(l2.players[0].pieces[0],
-                         (l1.players[0].pieces[0] + dice) % FIELDS)
-        self.assertEqual(l1.turn, l2.turn - 1)
+        self.assertEqual(l2.players[0].pieces[0], 6)
+        self.assertEqual(l2.turn, 1)
 
         l3, _ = l2.move(1, dice)
 
-        self.assertEqual(l3.players[1].pieces[1],
-                         (l2.players[1].pieces[1] + dice) % FIELDS)
-        self.assertEqual(l2.turn, l3.turn - 1)
+        self.assertEqual(l3.players[1].pieces[1], 20)
+        self.assertEqual(l3.turn, 2)
 
     def test_pass(self):
         l1 = self.get_ludo()
         l2, _ = l1.move(0, 0)
 
         self.assertEqual(l2.players[0].pieces[0], l1.players[0].pieces[0])
-        self.assertEqual(l1.turn, l2.turn - 1)
+        self.assertEqual(l2.turn, 1)
 
     def test_scored(self):
-        dice = random.randint(1, 6)
+        dice = 3
         l1 = self.get_ludo()
         l1.players[0].pieces[0] = FIELDS-1
         l2, _ = l1.move(0, dice)
 
-        self.assertEqual(l2.players[0].points, l1.players[0].points + 1)
+        self.assertEqual(l2.players[0].points, 1)
+        self.assertEqual(l2.players[0].pieces[0], 2)
         self.assertFalse(l2.players[0].points == l1.players[0].points)
 
     def test_finished(self):
-        dice = random.randint(1, 6)
+        dice = 6
         l1 = self.get_ludo()
         l1.players[0].pieces[0] = FIELDS - 1
         l1.players[0].points = MAX_POINTS - 1
